59360acfa6f7421f6d87336d313e98492591aea2
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-06-03 11:31:39 +0530

    Answer Submitted

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-export default App
+  const fetchData = async () => {
+    // console.log("fetching the data");
+    try {
+      const res = await fetch("https://dummyjson.com/products");
+      const data = await res.json();
+      setData(data.products);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  // console.log(data);
+  if (isLoading) return <pre>Loading...</pre>;
+  return (
+    data && (
+      <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    )
+  );
+};
+
+export default App;
